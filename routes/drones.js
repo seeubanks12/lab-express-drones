edbdede993d748abf2df8eaaca39cbcb007c884a
Eliza <seeubanks12@gmail.com>
2022-02-07 15:28:04 -0500

    iterations 3 and 4

--- routes/drones.js
+++ routes/drones.js
@@ -17,26 +17,58 @@ router.get('/drones', (req, res, next) => {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-  Drone.create({
-    name: req.params.name,
-    propellers: req.params.propellers,
-    maxSpeed: req.params.propellers
-  })
+  res.render('drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+Drones.create({
+  name: req.body.name,
+  propellers: req.body.propellers,
+  maxSpeed: req.body.maxSpeed
+}).then((results) => {
+console.log("Drone was created", results)
+res.redirect("/drones")
+}).catch(err => {
+  console.log ("Something went wrong", err)
 });
+});
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+const { id: droneId } = req.params;
+  console.log(droneId);
+  Drones.findById(droneId)
+    .then((foundDrone) => {
+      console.log("this is your drone: " + foundDrone);
+      res.render("drones/update-form.hbs", { drone: foundDrone });
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+const { id: droneId } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+  console.log(droneId, name, propellers, maxSpeed);
+  Drones.findOneAndUpdate(
+    { _id: droneId },
+    { name, propellers, maxSpeed },
+    { new: true }
+  )
+    .then(() => {
+      console.log("drone updated");
+      res.redirect("/drones");
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
